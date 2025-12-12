63ccef499c1a01c4a46716f955b1000c668cba01
Ralph Cajazeira <ralphmtk@gmail.com>
2025-12-12 20:19:32 -0300

    Up 1

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,12 @@
 import express from "express"
+import cors from "cors"
 
 const app = express()
+app.use(cors())
 app.use(express.json())
+
+app.get("/", (request, response) => {
+  return response.json({ message: "Hello Node.js!" })
+})
 
 export { app }

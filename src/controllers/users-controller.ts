75a6255ae5ba60057d4d8090d24264930e4ba6b9
Ralph Cajazeira <ralphmtk@gmail.com>
2025-12-15 15:51:31 -0300

    Rotas configuradas, criando usuário

---
+++ src/controllers/users-controller.ts
@@ -0,0 +1,18 @@
+import { Request, Response } from "express"
+import { email, z } from "zod"
+
+class UsersController {
+  async create(request: Request, response: Response) {
+    const bodySchema = z.object({
+      name: z.string(),
+      email: z.email(),
+      password: z.string().min(6),
+    })
+
+    const { name, email, password } = bodySchema.parse(request.body)
+
+    response.json({ name, email, password })
+  }
+}
+
+export { UsersController }
---
+++ src/routes/users-routes.ts
@@ -0,0 +1,14 @@
+import { Router } from "express"
+
+import { UsersController } from "@/controllers/users-controller"
+
+const usersRoutes = Router()
+const usersController = new UsersController()
+
+usersRoutes.post("/", usersController.create)
+// usersRoutes.get("/", usersController.list)
+// usersRoutes.get("/:id", usersController.show)
+// usersRoutes.put("/:id", usersController.update)
+// usersRoutes.delete("/:id", usersController.remove)
+
+export { usersRoutes }

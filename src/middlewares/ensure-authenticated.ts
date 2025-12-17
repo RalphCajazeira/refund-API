5d04ebae60e6b474ee3a2dc09a248edfcefcbe8a
Ralph Cajazeira <ralphmtk@gmail.com>
2025-12-17 18:37:54 -0300

    feat: apply JWT authentication to private routes

---
+++ src/middlewares/ensure-authenticated.ts
@@ -0,0 +1,44 @@
+import type { Request, Response, NextFunction } from "express"
+
+import { verify } from "jsonwebtoken"
+
+import { authConfig } from "@/configs/auth"
+import { AppError } from "@/utils/AppError"
+import th from "zod/v4/locales/th.js"
+
+interface TokenPayLoad {
+  role: string
+  sub: string
+}
+
+function ensureAuthenticated(
+  request: Request,
+  response: Response,
+  next: NextFunction
+) {
+  try {
+    const authHeader = request.headers.authorization
+
+    if (!authHeader) {
+      throw new AppError("Token not found", 401)
+    }
+
+    const [, token] = authHeader.split(" ")
+
+    const { role, sub: user_id } = verify(
+      token,
+      authConfig.jwt.secret
+    ) as TokenPayLoad
+
+    request.user = {
+      id: user_id,
+      role,
+    }
+
+    return next()
+  } catch (error) {
+    throw new AppError("Invalid token", 401)
+  }
+}
+
+export { ensureAuthenticated }
